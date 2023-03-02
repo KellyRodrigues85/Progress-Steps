212d8d0e7607cae841088cb0db88a2f946bf44bd
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-03-02 18:50:43 -0300

    click event

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-const circle = document.getElementById('circle')
+const circles = document.querySelectorAll('.circle')    
+
+let currentActive = 1
+
+next.addEventListener('click', () =>{
+    currentActive ++
+    
+    if(currentActive > circles.length){
+        currentActive = circles.length
+        
+    }
+
+    upadate()
+})
+
+prev.addEventListener('click', () =>{
+    currentActive --    
+    
+    if(currentActive < 1){
+        currentActive = 1
+        console.log(currentActive)
+    }
+
+    upadate()
+})
+
+function upadate{
+    
+}
